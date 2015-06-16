1f1ce9fca5d49a97a84bb1448d165ed071e924ae
Joe Reeve <joe@wigwamm.com>
2015-06-16 20:32:45 +0100

    INIt

---
+++ devtools.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.devtools.panels.elements.createSidebarPane(
+  "Ractive Data",
+  function(sidebar) {
+    function update() {
+      sidebar.setExpression("$0._ractive.root.get($0._ractive.keypath.str)");
+    }
+
+    update();
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(update);
+  }
+);
