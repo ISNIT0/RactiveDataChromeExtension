f577e7812d9965dc4568092fd6d7ebcb7c431497
Chris Reeves <evschris@gmail.com>
2016-03-05 02:39:11 -0500

    slightly more tolerant detection of 0.8

--- devtools.js
+++ devtools.js
@@ -6,7 +6,7 @@ chrome.devtools.panels.elements.createSidebarPane(
   "Ractive Data",
   function(sidebar) {
     function update() {
-      sidebar.setExpression("!$0._ractive ? { info: 'Pick a Ractive managed node to see the context.' } : $0._ractive.context ? $0._ractive.context.get() : $0._ractive.root.get($0._ractive.keypath.str)");
+      sidebar.setExpression("!$0._ractive ? { info: 'Pick a Ractive managed node to see the context.' } : $0._ractive.context ? $0._ractive.context.get() : $0._ractive.root ? $0._ractive.root.get($0._ractive.keypath.str) : $0._ractive.ractive ? $0._ractive.ractive.get($0._ractive.keypath) : { info: \'It looks like this is an incompatible Ractive version.\' }");
     }
 
     update();
